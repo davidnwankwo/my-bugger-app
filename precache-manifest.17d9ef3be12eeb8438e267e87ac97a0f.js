3bd85f3528113f9019d37365dae621937df50529
davidnwankwo <53377662+davidnwankwo@users.noreply.github.com>
2020-03-02 05:25:55 -0800

    build version 1.0

---
+++ precache-manifest.17d9ef3be12eeb8438e267e87ac97a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9126a0e3a50287a5ecfbc2297ffa454f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f24bba702dc200f0371a",
+    "url": "/static/css/main.0dc50bab.chunk.css"
+  },
+  {
+    "revision": "a25fc2bdf270b9253039",
+    "url": "/static/js/2.a8f07380.chunk.js"
+  },
+  {
+    "revision": "f24bba702dc200f0371a",
+    "url": "/static/js/main.93cdae00.chunk.js"
+  },
+  {
+    "revision": "bea673e5d95166cf15d1",
+    "url": "/static/js/runtime-main.4a02309f.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
